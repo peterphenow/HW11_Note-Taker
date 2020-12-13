4d7dd28619c0bcfe65d630cbf41af1b51d86cf7b
Peter Phenow <peter.phenow@gmail.com>
2020-12-12 21:29:22 -0600

    corrected wildcard route

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.get("/notes", function (req, res) {
 });
 
 // wildcard route to send user to "home" page
-app.get("/*", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
